41b7f145cde08dfd57e831f1a6b56a517ab3c4a1
Osa <osaimola@gmail.com>
2020-09-03 21:12:18 -0600

    Add Drawing class; create some tests in Program.cs;

---
+++ OOP_SHAPES_INHRTNCE_POLYMPSM_ABSTRCTN_ENCAPSTN_CSHARP/Drawing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OOP_SHAPES_INHRTNCE_POLYMPSM_ABSTRCTN_ENCAPSTN_CSHARP.Shapes;
+using System.Linq;
+
+namespace OOP_SHAPES_INHRTNCE_POLYMPSM_ABSTRCTN_ENCAPSTN_CSHARP
+{
+    class Drawing
+    {
+        public double SpaceCovered { get => Shapes.Select(x => x.Area).Sum(); }
+        public double LinesDrawn
+        { 
+            get 
+            {
+                // use linq Where to filter for Triangles/Rectangles/Circles
+                // use linq select to return type casted objects for each filtered list
+                // use linq select to return the perimeters/circumference of each object
+                // use the linq sum to get the sums
+                // return the sum of sums to complete the LinesDrawn get
+                return (
+                    Shapes.Where(x => x.GetType() == typeof(Triangle)).Select(x => (Triangle)x).Select(x => x.Perimeter).Sum()
+                    + Shapes.Where(x => x.GetType() == typeof(Rectangle)).Select(x => (Rectangle)x).Select(x => x.Perimeter).Sum()
+                    + Shapes.Where(x => x.GetType() == typeof(Circle)).Select(x => (Circle)x).Select(x => x.Circumference).Sum()
+                    );
+                    
+            } 
+        }
+
+        private List<Shape> Shapes { get; set; }
+
+        public Drawing(List<Shape> shapes)
+        {
+            Shapes = shapes;
+        }
+
+        public void Draw(Shape shape)
+        {
+            Shapes.Add(shape);
+        }
+
+        public override string ToString()
+        {
+            double redArea = Shapes.Where(x => x.Color == Color.Red).Select(x => x.Area).Sum();
+            double blueArea = Shapes.Where(x => x.Color == Color.Blue).Select(x => x.Area).Sum();
+            double greenArea = Shapes.Where(x => x.Color == Color.Green).Select(x => x.Area).Sum();
+            return $"A drawing consisting of {Shapes.ToArray().Length} Shapes. It is {redArea/SpaceCovered*100:F2}% red, {blueArea / SpaceCovered * 100:F2}% blue, and {greenArea / SpaceCovered * 100:F2}% green";
+        }
+    }
+}
--- OOP_SHAPES_INHRTNCE_POLYMPSM_ABSTRCTN_ENCAPSTN_CSHARP/Program.cs
+++ OOP_SHAPES_INHRTNCE_POLYMPSM_ABSTRCTN_ENCAPSTN_CSHARP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using OOP_SHAPES_INHRTNCE_POLYMPSM_ABSTRCTN_ENCAPSTN_CSHARP.Shapes;
 
 namespace OOP_SHAPES_INHRTNCE_POLYMPSM_ABSTRCTN_ENCAPSTN_CSHARP
@@ -7,9 +8,37 @@ namespace OOP_SHAPES_INHRTNCE_POLYMPSM_ABSTRCTN_ENCAPSTN_CSHARP
     {
         static void Main(string[] args)
         {
+            // create three colored shapes
             Shape myCircle = new Circle(5, Color.Red);
             Shape myRectangle = new Rectangle(5, 10, Color.Blue);
             Shape myTriangle = new Triangle(5, 9, Color.Green);
+
+            // POLYMORPHISM: here our listOfShapes diplayes polymorphism as all shapes are acting as Object type 'Shape' as well
+            // as Circle, Rectangel, and Triangle..
+            // create a list of shapes to add to our drawing
+            List<Shape> listOfShapes = new List<Shape> { myCircle, myRectangle, myTriangle };
+
+            // create a drawing and call the tostring into the console.
+            // Expect: A drawing consisting of 3 Shapes. It is 52.00% red, 33.10% blue, and 14.90% green
+            Drawing myDrawing = new Drawing(listOfShapes);
+            Console.WriteLine(myDrawing);
+
+            // Expect: 85.7115
+            Console.WriteLine($"Lines Drawn: {myDrawing.LinesDrawn}");
+            // Expect: 151.0397
+            Console.WriteLine($"Space Covered: {myDrawing.SpaceCovered}");
+
+
+            Shape anotherRectangle = new Rectangle(59.44, 13.89, Color.Red);
+            myDrawing.Draw(anotherRectangle);
+            Shape anotherTriangle = new Triangle(5.56, 69.44, Color.Blue);
+            myDrawing.Draw(anotherTriangle);
+            Shape anotherCircle = new Circle(46, Color.Green);
+            myDrawing.Draw(anotherCircle);
+
+            Console.WriteLine("\n\nAdding 3 more drawings...");
+            Console.WriteLine(myDrawing);
+
         }
     }
 }
