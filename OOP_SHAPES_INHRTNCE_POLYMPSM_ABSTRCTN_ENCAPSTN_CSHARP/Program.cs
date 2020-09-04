ab0a2afa967c4ff7c75dc09da798bbf98c39dece
Osa <osaimola@gmail.com>
2020-09-03 19:09:53 -0600

    Add project files.

---
+++ OOP_SHAPES_INHRTNCE_POLYMPSM_ABSTRCTN_ENCAPSTN_CSHARP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_SHAPES_INHRTNCE_POLYMPSM_ABSTRCTN_ENCAPSTN_CSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
